316537017ec44b2efd2062896f4241b83077b712
ejagojo <agojo.eljohn@gmail.com>
2025-03-24 16:54:46 -0400

    Added the Showcase Rewards system and routes users to loyalty page

--- src/pages/Loyalty.jsx
+++ src/pages/Loyalty.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import Header from "../components/Header"
 
 const Loyalty = () => {
-    return (
-      <div className="flex items-center justify-center h-screen text-black">
-        <Header />
+  return (
+    <div className="relative w-full h-screen text-black">
+      <Header />
+      <div className="pt-24 flex items-center justify-center h-full">
         <h1 className="text-4xl font-bold knewave-font">Loyalty - Placeholder</h1>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default Loyalty
+export default Loyalty;
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import SubNav from "../components/SubNav";
+import RewardShowcase from "../components/RewardShowcase";
 
 const Order = () => {
   return (
     <div className="relative w-full h-screen text-black">
       <Header />
       <SubNav/>
+      <RewardShowcase />
       <div className="pt-24 flex flex-col items-center justify-center h-full">
         <h1 className="text-4xl font-bold">Order Page - Placeholder</h1>
       </div>
