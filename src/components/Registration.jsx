bf8f7ea87e8f18f91d61d4c35046e5e8cb1a70e6
ejagojo <agojo.eljohn@gmail.com>
2025-04-01 22:53:49 -0400

    Added new first name and last name in registration form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -7,6 +7,8 @@ import LoadingSpinner from "./LoadingSpinner";
 
 const SignUpForm = ({ toggleForm }) => {
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
@@ -40,48 +42,70 @@ const SignUpForm = ({ toggleForm }) => {
   return (
     <>
       {loading && <LoadingSpinner />}
-      <div className="w-full max-w-[320px] md:max-w-[400px] p-4 border border-white rounded-lg bg-opacity-0 backdrop-blur-xs">
+      <div className="w-full max-w-[320px] md:max-w-[600px] p-4 border border-white rounded-lg bg-opacity-0 backdrop-blur-xs">
         <h2 className="text-lg font-semibold mb-4 text-center">Sign Up</h2>
-        <div className="mb-3">
-          <label className="block text-xs mb-1">Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
-            placeholder="Email"
-          />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+          <div>
+            <label className="block text-xs mb-1">First Name</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+              placeholder="First Name"
+            />
+          </div>
+          <div>
+            <label className="block text-xs mb-1">Last Name</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+              placeholder="Last Name"
+            />
+          </div>
+          <div>
+            <label className="block text-xs mb-1">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <label className="block text-xs mb-1">Date of Birth</label>
+            <input
+              type="date"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+            />
+          </div>
+          <div>
+            <label className="block text-xs mb-1">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <label className="block text-xs mb-1">Re-Type Password</label>
+            <input
+              type="password"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+              className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
+              placeholder="Re-type password"
+            />
+          </div>
         </div>
-        <div className="mb-3">
-          <label className="block text-xs mb-1">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="block text-xs mb-1">Re-Type Password</label>
-          <input
-            type="password"
-            value={password2}
-            onChange={(e) => setPassword2(e.target.value)}
-            className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
-            placeholder="Re-type password"
-          />
-        </div>
-        {error && <p className="text-red-500 text-xs text-center mb-2">{error}</p>}
-        <div className="mb-3">
-          <label className="block text-xs mb-1">Date of Birth</label>
-          <input
-            type="date"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
-            className="w-full px-3 py-2 text-black bg-white rounded-md focus:outline-none font-medium border border-gray-300"
-          />
-        </div>
+        {error && <p className="text-red-500 text-xs text-center mt-2">{error}</p>}
         <button
           onClick={handleSignUp}
           className={`w-full px-4 py-3 mt-4 text-sm font-semibold rounded-md transition-transform duration-300 ease-in-out ${
